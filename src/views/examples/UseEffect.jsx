31d138f368a4941d4f13ebdecd027dedeaa5dbb2
JoaquimMoura <joaquim.castro.moura@gmail.com>
2022-05-24 17:18:35 -0300

    Desafio UseEffect - Resposta.

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle';
 
 function calcFatorial(num) {
 
@@ -11,6 +12,8 @@ function calcFatorial(num) {
 
 const UseEffect = (props) => {
 
+    // EX 01
+
     const [number, setNumber] = useState(1);
     const [fatorial, setFatorial] = useState(1);
 
@@ -18,13 +21,21 @@ const UseEffect = (props) => {
         setFatorial(calcFatorial(number))
     }, [number]);
 
-    useEffect(function() {
-        if(fatorial > 1000) {
+    useEffect(function () {
+        if (fatorial > 1000) {
             document.title = 'Joaquim estamos no caminho certo!'
         } else {
             document.title = 'Bora!!!'
         }
-    }, [fatorial])
+    }, [fatorial]);
+
+    // EX 02
+    const [status, setStatus] = useState("Impar");
+
+    useEffect(function () {
+        setStatus(number % 2 === 0 ? "Par" : "Impar")
+    }, [number])
+
 
     return (
         <div className="UseEffect">
@@ -32,6 +43,7 @@ const UseEffect = (props) => {
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectionTitle title="Exercicio #01" />
             <div className="center">
                 <div>
                     <span className="text">Fatorial: </span>
@@ -41,6 +53,14 @@ const UseEffect = (props) => {
                     className="input"
                     value={number}
                     onChange={e => setNumber(e.target.value)} />
+            </div>
+
+            <SectionTitle title="Exercicio #02" />
+            <div className="center">
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">{status}</span>
+                </div>
             </div>
         </div>
     )
